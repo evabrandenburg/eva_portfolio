d565457f9610820f3beefbb2b166c5f34d54de31
evabrandenburg <evabrandenburg89@gmail.com>
2014-05-26 21:58:15 -0400

    added pic

--- scroll portfolio/javascript/main.js
+++ scroll portfolio/javascript/main.js
@@ -33,17 +33,7 @@ function init() {
 		.rotate(3*Math.PI/2, {
 			name: "rotations-rotated"
 		})
-		// Continue upwards to 'source'
-		.lineTo(2400, -700, {
-			name: "source"
-		})
-		// Small arc downwards
-		.arc(2250, -700, 150, 0, -Math.PI/2, true)
-
-		//Line to 'follow'
-		.lineTo(1350, -850, {
-			name: "follow"
-		})
+	
 		// Arc and rotate back to the beginning.
 		.arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2, name: "end"});
 
