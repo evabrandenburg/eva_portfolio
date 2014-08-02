1211976027c614afa321790e55ebec61b5352acd
evabrandenburg <evabrandenburg89@gmail.com>
2014-08-02 16:14:09 -0400

    mo

--- box/javascript/main.js
+++ box/javascript/main.js
@@ -1,3 +1,13 @@
+  var collection = $("div.container div").get();
+    collection.sort(function() {
+        return Math.random()*10 > 5 ? 1 : -1;
+    });
+    $.each(collection,function(i,el) {
+        var color = this.className,
+            $el = $(el);
+        $el.css({backgroundColor: color}).appendTo( $el.parent() );
+    });
+
 
 $("div").click( show );
 
