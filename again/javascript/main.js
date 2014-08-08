bdde00282c04172b50cde1b2feec06bc95a1c6a6
evabrandenburg <evabrandenburg89@gmail.com>
2014-08-07 22:22:30 -0400

    more

--- again/javascript/main.js
+++ again/javascript/main.js
@@ -1,8 +1,24 @@
 /* CURRENTLY IN: javascript/main.js */
 
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        verticalCentered: true,
+        resize : true,
+        scrollingSpeed: 700,
+        easing: 'easeInQuart',
+        autoScrolling: false
+     });
+});
+
 function show (){
 	$( ".ball3" ).addClass( "show" );
 }
 
 $( ".thought" ).hover( show );
+
+function noG(){
+	$( ".me" ).addClass( "noG" );
+}
+
+$( ".me" ).hover( noG );
 
