821a1ee80695fd1c274aa91acb96f338c6bf8f25
evabrandenburg <evabrandenburg89@gmail.com>
2014-08-08 16:55:50 -0400

    mo

--- again/javascript/main.js
+++ again/javascript/main.js
@@ -20,15 +20,15 @@ function noG(){
 	$( ".me" ).addClass( "noG" );
 }
 
-/*$( ".me" ).hover( noG );
+$( ".me" ).hover( noG );
 
-//
+
 function look1(){
 	$( ".me" ).addClass( "look1" );
 }
 $(".b1").hover( look1 );
 
-//
+/*
 function look2(){
 	$( ".me" ).addClass( "look2" );
 }
@@ -38,13 +38,13 @@ $(".b2").hover( look2 );
 /*function look3(){
 	$( ".me" ).addClass( "look3" );
 }
-$(".b3").hover( look3 );*/
+$(".b3").hover( look3 );
 $(".b1").mouseover(function(){
-         $(".me").css("background-image", "(url../pics/LookL.jpg)");
+         $(".me").css("background-image", "url(../../pics/LookL.jpg)");
 });
 
 $(".b1").mouseout(function(){
-         $(".me").css("background-image", "(url../pics/smile.jpg)");
+         $(".me").css("background-image", "url(../../pics/smile.jpg)");
 });
 
 $(".b2").mouseover(function(){
@@ -63,6 +63,8 @@ $(".b3").mouseover(function(){
 $(".b3").mouseout(function(){
          $(".me").css("background-color", "blue");
 });
+
+*/
 
 
 
